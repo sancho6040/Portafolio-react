be79264bbccbbb2aa1c56c8e8d1413c1432986c8
sancho6040 <qwetito@gmail.com>
2022-09-18 22:14:39 -0500

    Create Project

---
+++ src/Pages/PortafolioPage.jsx
@@ -0,0 +1,6 @@
+// Creación de arreglo de proyectos para leer en sección portafolio
+// Creación de mínimo 4 proyectos
+        // Aplicación TODO List (obligatorio)
+        // Aplicación de Héroes (obligatorio)
+        // Aplicación Clima (obligatorio)
+        // Aplicación Landing Page (obligatorio)
